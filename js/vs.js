e2cd5025005f283b766229dda089fefd006c5d54
Victoria Shi <victoria.shi@berkeley.edu>
2015-03-24 10:54:48 -0700

    portfolio v2 up

---
+++ js/vs.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+    $("img[class^='scroll-']").on('click', function(e) {
+    // prevents the default action of the link
+    e.preventDefault();
+    // assigns the text of the clicked-link to a variable for comparison purposes
+    var currentClass = $(this).attr('class');
+    
+    if (currentClass == "scroll-u"){
+        $('html, body').animate({
+        scrollTop: $("#navbar").offset().top
+    }, 500);
+    }
+    else if (currentClass == "scroll-r"){
+        
+    }else if (currentClass == "scroll-l"){
+        
+    }else{
+        
+    }
+});
+
+});
+
