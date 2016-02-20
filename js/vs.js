227e9b259d2a6b77460d647250f4b52703cf5529
Victoria Shi <victoria.shi@berkeley.edu>
2016-02-20 05:25:12 -0800

    add 100 days of design

--- js/vs.js
+++ js/vs.js
@@ -29,3 +29,15 @@ $(document).ready(function(){
        
     });
 });
+$(window).load(function(){
+    console.log("hello");
+    var $grid = $('.masonry-grid').masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    });
+// layout Isotope after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry();
+    });  
+});
